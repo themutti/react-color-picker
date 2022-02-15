92d01d2f7a384a34a8088d01f6639aa3a99fc49a
imperialvictor <imperialvictor15@gmail.com>
2022-02-15 23:36:17 +0100

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="header-inner container">
+        <h1 className="header-title">COLOR PICKER</h1>
+      </div>
+    </header>
+  );
+}
