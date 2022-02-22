114fcf7063b93ec8b91d053c73f477d96d3f2d47
imperialvictor <imperialvictor15@gmail.com>
2022-02-22 23:45:47 +0100

    Defined all 12 colors and added a border to them

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,20 +1,56 @@
 const colors = [
   {
+    name: "Black",
+    red: 0,
+    green: 0,
+    blue: 0
+  },
+  {
+    name: "White",
+    red: 255,
+    green: 255,
+    blue: 255
+  },
+  {
     name: "Red",
     red: 255,
     green: 0,
     blue: 0
   },
   {
+    name: "Orange",
+    red: 255,
+    green: 106,
+    blue: 0
+  },
+  {
+    name: "Yellow",
+    red: 255,
+    green: 216,
+    blue: 0
+  },
+  {
     name: "Green",
+    red: 0,
+    green: 204,
+    blue: 0
+  },
+  {
+    name: "Lime",
     red: 0,
     green: 255,
     blue: 0
   },
   {
-    name: "Blue",
+    name: "Acqua",
     red: 0,
-    green: 0,
+    green: 255,
+    blue: 255
+  },
+  {
+    name: "Light blue",
+    red: 0,
+    green: 148,
     blue: 255
   },
   {
@@ -24,52 +60,16 @@ const colors = [
     blue: 255
   },
   {
-    name: "Blue",
-    red: 0,
+    name: "Purple",
+    red: 178,
     green: 0,
     blue: 255
   },
   {
-    name: "Blue",
-    red: 0,
+    name: "Pink",
+    red: 255,
     green: 0,
-    blue: 255
-  },
-  {
-    name: "Blue",
-    red: 0,
-    green: 0,
-    blue: 255
-  },
-  {
-    name: "Blue",
-    red: 0,
-    green: 0,
-    blue: 255
-  },
-  {
-    name: "Blue",
-    red: 0,
-    green: 0,
-    blue: 255
-  },
-  {
-    name: "Blue",
-    red: 0,
-    green: 0,
-    blue: 255
-  },
-  {
-    name: "Blue",
-    red: 0,
-    green: 0,
-    blue: 255
-  },
-  {
-    name: "Blue",
-    red: 0,
-    green: 0,
-    blue: 255
+    blue: 220
   }
 ];
 
